a1686de3138a7b83371a936a4a7c367a05ee19b7
Crystal Csete <jeannecc13@gmail.com>
2021-05-14 16:22:28 -0600

    integrated antd into components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Nav from "./Components/Nav";
+import Home from "./Components/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="app__container">
-      <Nav />
+      <Home />
     </div>
   );
 }
